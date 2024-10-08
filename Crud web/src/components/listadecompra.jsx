3db37cb0e8096bea07013242696a775133108126
marcho2 <103913996+marcho2@users.noreply.github.com>
2024-10-08 12:05:36 -0300

    Update listadecompra.jsx
    Actualizacion de lista de compras.jsx, donde se añade la funcion de eliminar compra, y links con react-router-dom para compraCreate

--- Crud web/src/components/listadecompra.jsx
+++ Crud web/src/components/listadecompra.jsx
@@ -1,28 +1,56 @@
-import React from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const ListaDeCompras = ({ compras }) => {
-  return (
-    <table className="table table-striped-columns">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Producto</th>
-          <th>Precio</th>
-          <th>Fecha de Ingreso</th>
-        </tr>
-      </thead>
-      <tbody>
-        {compras.map((compra) => (
-          <tr key={compra.id}>
-            <td>{compra.id}</td>
-            <td>{compra.producto}</td>
-            <td>${compra.precio}</td>
-            <td>{compra.fecha_ingreso}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+const ListaDeCompras = ({ compras, setCompras }) => {
+    const removeCompra = (id) => {
+        if (window.confirm('¿Desea eliminar esta compra?')) {
+            const nuevasCompras = compras.filter(compra => compra.id !== id);
+            setCompras(nuevasCompras);
+        }
+    }
+
+    return (
+        <div className="container">
+            <div className="card">
+                <div>
+                    <h1>Listado de Compras</h1>
+                </div>
+                <div className="card-body">
+                    <div className="divbtn">
+                        <Link to="compra/create" className="btn btn-success">Agregar Nueva</Link>
+                    </div>
+                    <table className="table table-bordered">
+                        <thead className="bg-dark text-white">
+                            <tr>
+                                <td>ID</td>
+                                <td>Producto</td>
+                                <td>Precio</td>
+                                <td>Fecha de Ingreso</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {compras &&
+                                compras.map(compra => (
+                                    <tr key={compra.id}>
+                                        <td>{compra.id}</td>
+                                        <td>{compra.producto}</td>
+                                        <td>{compra.precio}</td>
+                                        <td>{compra.fechaIngreso}</td>
+                                        <td>
+                                            <button onClick={() => removeCompra(compra.id)} className="btn btn-danger">Eliminar</button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default ListaDeCompras;
