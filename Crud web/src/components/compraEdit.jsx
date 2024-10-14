151f599bc2915d590c33dd01a78fe03e3f24057c
marcho2 <103913996+marcho2@users.noreply.github.com>
2024-10-14 19:34:00 -0300

    Update compraEdit.jsx

--- Crud web/src/components/compraEdit.jsx
+++ Crud web/src/components/compraEdit.jsx
@@ -16,4 +16,65 @@ const CompraEdit = ({ compras, setCompras }) => {
             fechaIngresoChange(compraExistente.fechaIngreso);
         }
     }, [compraExistente]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const compraActualizada = {
+            id: parseInt(id),
+            producto,
+            precio,
+            fechaIngreso,
+        };
+        const nuevasCompras = compras.map(compra => compra.id === parseInt(id) ? compraActualizada : compra);
+        setCompras(nuevasCompras);
+        alert('Compra actualizada exitosamente.');
+        navigate('/');
+    }
+    return (
+        <div className="row">
+            <div className="offset-lg-3 col-lg-6">
+                <form className="container" onSubmit={handleSubmit}>
+                    <div className="card" style={{ textAlign: "left" }}>
+                        <div className="card-title">
+                            <h2>Editar Compra</h2>
+                        </div>
+                        <div className="card-body">
+                            <div className="row">
+
+                                <div className="col-lg-12">
+                                    <div className="form-group">
+                                        <label>Producto</label>
+                                        <input required value={producto} onChange={e => productoChange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-12">
+                                    <div className="form-group">
+                                        <label>Precio</label>
+                                        <input type="number" value={precio} onChange={e => precioChange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-12">
+                                    <div className="form-group">
+                                        <label>Fecha de Ingreso</label>
+                                        <input type="date" value={fechaIngreso} onChange={e => fechaIngresoChange(e.target.value)} className="form-control"></input>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-12">
+                                    <div className="form-group">
+                                        <button className="btn btn-success" type="submit">Guardar</button>
+                                        <Link to="/" className="btn btn-danger">Volver</Link>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
+
+export default CompraEdit;
