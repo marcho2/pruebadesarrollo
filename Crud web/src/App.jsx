e8d1a1faab3abb7f6475a5386901d1b9f27f344e
marcho2 <103913996+marcho2@users.noreply.github.com>
2024-10-16 11:01:32 -0300

    Update App.jsx

--- Crud web/src/App.jsx
+++ Crud web/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from "axios";
 import './App.css';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
@@ -7,19 +8,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ListaDeCompras from './components/listadecompras';
 import CompraCreate from './components/compraCreate';
+import CompraEdit from './components/compraEdit';
 
 function App() {
-    const [compras, setCompras] = useState([]);
+    const [compras, setCompras] = useState([]); // array donde se guardan las compras
 
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={< ListaDeCompras compras={compras} setCompras={setCompras} />}></Route>
-                    <Route path='/compra/create' element={<CompraCreate compras={compras} setCompras={setCompras} />}></Route>
+                    <Route path='/' element={<ListaDeCompras compras={compras} setCompras={setCompras} />} />
+                    <Route path='/compra/create' element={<CompraCreate compras={compras} setCompras={setCompras} />} />
                     <Route path='/compra/edit/:id' element={<CompraEdit compras={compras} setCompras={setCompras} />} />
                 </Routes>
             </BrowserRouter>
+
         </div>
     );
 }
