db73a57e028ae7a22b5e10bba0f404d7e61491d0
marcho2 <103913996+marcho2@users.noreply.github.com>
2024-10-16 01:18:11 -0300

    Update compraCreate.jsx
    Introduccion de Mockapi y solucion de algunos errores

--- Crud web/src/components/compraCreate.jsx
+++ Crud web/src/components/compraCreate.jsx
@@ -1,31 +1,33 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const CompraCreate = ({ compras, setCompras }) => {
+const CompraCreate = () => {
     const [producto, productoChange] = useState("");
     const [precio, precioChange] = useState("");
-    const [validation, valChange] = useState(false)
     const navigate = useNavigate();
+
     const handleSubmit = (e) => {
         e.preventDefault();
-
         const currentDateTime = new Date();
         const dia = currentDateTime.getDate();
         const mes = currentDateTime.getMonth() + 1;
         const año = currentDateTime.getFullYear();
-        const fechaIngreso = `${dia < 10 ? '0' : ''}${dia}/${mes < 10 ? '0' : ''}${mes}/${año}`;
+        const fechaIngreso = `${año}-${mes < 10 ? '0' : ''}${mes}-${dia < 10 ? '0' : ''}${dia}`;
 
         const nuevaCompra = {
-            id: compras.length + 1,
             producto,
             precio,
-            fechaIngreso,
+            fechaIngreso
         };
-        setCompras([...compras, nuevaCompra]);
 
-        alert('Compra guardada exitosamente.');
-        navigate('/');
+        axios.post('https://670dde27073307b4ee44b5e1.mockapi.io/productos/productos', nuevaCompra)
+            .then(() => {
+                alert('Compra guardada exitosamente.');
+                navigate('/');
+            })
+            .catch(error => console.log(error));
     }
 
     return (
@@ -37,27 +39,16 @@ const CompraCreate = ({ compras, setCompras }) => {
                             <h2>Crear Compra</h2>
                         </div>
                         <div className="card-body">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <label>Producto</label>
-                                        <input required value={producto} onMouseDown={e => valChange(true)} onChange={e => productoChange(e.target.value)} className="form-control"></input>
-                                        {producto.length === 0 && validation && <span className="text-danger">Ingrese el nombre del producto</span>}
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <label>Precio</label>
-                                        <input type="number" value={precio} onChange={e => precioChange(e.target.value)} className="form-control"></input>
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <button className="btn btn-success" type="submit">Guardar</button>
-                                        <Link to="/" className="btn btn-danger">Volver</Link>
-                                    </div>
-                                </div>
+                            <div className="form-group">
+                                <label>Producto</label>
+                                <input required value={producto} onMouseDown={e => valChange(true)} onChange={e => productoChange(e.target.value)} className="form-control"></input>
                             </div>
+                            <div className="form-group">
+                                <label>Precio</label>
+                                <input type="number" required value={precio} onMouseDown={e => valChange(true)} onChange={e => precioChange(e.target.value)} className="form-control"></input>
+                            </div>
+                            <button className="btn btn-success" type="submit">Guardar</button>
+                            <Link to="/" className="btn btn-danger">Volver</Link>
                         </div>
                     </div>
                 </form>
