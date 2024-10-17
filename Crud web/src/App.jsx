69bb621385145b6dc6584e5edcc390e54ed546a7
Sebastian Marchant <103913996+marcho2@users.noreply.github.com>
2024-10-16 23:25:24 -0300

    Update App.jsx

--- Crud web/src/App.jsx
+++ Crud web/src/App.jsx
@@ -5,7 +5,7 @@ import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter ,BrowserRouter, Route, Routes } from 'react-router-dom';
 import ListaDeCompras from './components/listadecompras';
 import CompraCreate from './components/compraCreate';
 import CompraEdit from './components/compraEdit';
@@ -15,14 +15,13 @@ function App() {
 
     return (
         <div className="App">
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path='/' element={<ListaDeCompras compras={compras} setCompras={setCompras} />} />
                     <Route path='/compra/create' element={<CompraCreate compras={compras} setCompras={setCompras} />} />
                     <Route path='/compra/edit/:id' element={<CompraEdit compras={compras} setCompras={setCompras} />} />
                 </Routes>
-            </BrowserRouter>
-
+            </HashRouter>
         </div>
     );
 }
